1213658ccb50428a394eb5955e1d027e7893deb2
Ryan Greuel <ryangreuel@gmail.com>
2017-07-15 23:16:19 -0500

    complete lesson 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'my-app', // to use me, do this in html <my-app (click)="colorChange()">Toggle Color</my-app>
+  template: `
+    <h1>{{name}}</h1>
+    <p>{{name}} is in the {{region}} region</p>
+    <br/>
+    <button (click)="addressClick()">Show/Hide address</button>
+    <div [hidden]="hideAddress">
+      <fieldset>
+        <label>Street: </label>{{street}}
+      </fieldset>
+      <fieldset>
+        <label>City: </label>{{city}}
+      </fieldset>
+      <fieldset>
+        <label>Region:</label>
+        <select (change)="regionChange($event.target.value)">
+          <option>East</option>
+          <option>North</option>
+          <option>South</option>
+          <option>West</option>
+        </select>
+      </fieldset>
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Alex Smith';
+  image = 'favicon.ico';
+  color = 'red';
+  street = '123 Ohio Ave';
+  city = 'Milwaukee';
+  region = 'East';
+  hideAddress = false;
+
+  addressClick() {
+    this.hideAddress = !this.hideAddress;
+  }
+
+  regionChange(region: string) {
+    this.region = region;
+  }
+}
